0c572a925cb4f3898121afbaf11b87ea0215ffb1
tiagoo <thurstaltamirano@escuelasproa.edu.ar>
2023-09-15 15:00:51 -0300

    .

--- Tarea/src/componentes/MyCards.jsx
+++ Tarea/src/componentes/MyCards.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 function MyCard({ autos }) {
   const cardItems = autos.map((auto) => {
     const [count, setCount] = useState(0); // Estado local para cada tarjeta
-
+    const [autosData, setAutosData] = useState(autos);
     function handleClick() {
       setCount(count + 1);
     }
+    function handleElim(id) {
+      // Filtramos los autos para eliminar el que tiene el ID igual al que pasamos
+      const updatedAutos = autosData.filter((auto) => auto.id !== id);
+      setAutosData(updatedAutos);
+
+      console.log('Entro ' + id)
+     
+      console.log(updatedAutos)
+    }
+
     return (
       <div key={auto.id} className="card">
         <div className="card-content">
@@ -17,6 +27,10 @@ function MyCard({ autos }) {
         <button onClick={handleClick}>
           Le diste like {count} veces
         </button>
+        <button  className="btnElim" onClick={() => handleElim(auto.id)}>
+          Eliminar
+        </button>
+
       </div>
     );
   });
